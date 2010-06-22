7f4f8fd3c3dfa0b71d3e1ef1c4d5563e4ec981a2
Tamas Nepusz <ntamas@gmail.com>
2010-06-22 15:44:55 +0100

    fixed download_names.py to work even if gfam is not on the Python path

--- bin/download_names.py
+++ bin/download_names.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
-from gfam.scripts.download_names import DownloadNamesApp
 import sys
+
+try:
+    from gfam.scripts.download_names import DownloadNamesApp
+except ImportError:
+    # Insert the parent directory of the master script into the Python path
+    # and try again
+    from os.path import dirname, join
+    sys.path.insert(0, join(dirname(sys.modules[__name__].__file__), ".."))
+    from gfam.scripts.download_names import DownloadNamesApp
 
 if __name__ == "__main__":
     sys.exit(DownloadNamesApp().run())
